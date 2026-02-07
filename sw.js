fff4f35e7ab87f8f94d1cbca68284525437b67e6
markusorngren <markus.orngren@gmail.com>
2026-02-07 17:50:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = 'mouse-tracker-v1';
+const CACHE_NAME = 'mouse-tracker-v3'; // Ändra detta nummer varje gång du gör stora ändringar!
 const ASSETS = [
   './',
-  './index.html', // Se till att din HTML-fil heter index.html
+  './index.html',
+  './manifest.json',
+  './icon.png',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
+// Installerar och sparar filer i cachen
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting(); // Tvingar den nya versionen att ta över direkt
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
+// Rensar gamla cachar när en ny version aktiveras
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Hämtar filer
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
