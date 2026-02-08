112364d1a15a1cb8e39fc7ea72457cea0dbf6f09
markusorngren <markus.orngren@gmail.com>
2026-02-08 10:28:36 +0100

    Add files via upload
    Knappfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouse-tracker-v12'; // Ändra detta nummer varje gång du gör stora ändringar!
+const CACHE_NAME = 'mouse-tracker-v13'; // Ändra detta nummer varje gång du gör stora ändringar!
 const ASSETS = [
   './',
   './index.html',
