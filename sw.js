c5bce030541a86e3a82605adcd32a98942349979
markusorngren <markus.orngren@gmail.com>
2026-02-09 19:50:43 +0100

    Add files via upload
    Beräkna väg via waypoint

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouse-tracker-v25'; // Uppdaterat nummer för att tvinga fram refresh på iPhone
+const CACHE_NAME = 'mouse-tracker-v26'; // Uppdaterat nummer för att tvinga fram refresh på iPhone
 const ASSETS = [
   './',
   './index.html',
@@ -43,7 +43,4 @@ self.addEventListener('message', (event) => {
       version: versionNumber
     });
   }
-
 });
-
-
