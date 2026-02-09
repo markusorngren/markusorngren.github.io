c5facbf0cf0ca95f848ce03d0ea221a7717ef295
markusorngren <markus.orngren@gmail.com>
2026-02-09 22:26:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouse-tracker-v29'; // Uppdaterat nummer för att tvinga fram refresh på iPhone
+const CACHE_NAME = 'mouse-tracker-v31'; // Ändrad version för att tvinga fram update
 const ASSETS = [
   './',
   './index.html',
@@ -8,15 +8,13 @@ const ASSETS = [
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-// Installerar och sparar filer i cachen
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Tvingar den nya versionen att ta över direkt
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Rensar gamla cachar när en ny version aktiveras
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
@@ -27,20 +25,15 @@ self.addEventListener('activate', (e) => {
   );
 });
 
-// Hämtar filer
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
 
-// Lyssnar efter meddelanden från appen för att skicka versionsnummer
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'GET_VERSION') {
-    // Rensar namnet så att bara versionsnumret skickas tillbaka
     const versionNumber = CACHE_NAME.split('-').pop(); 
-    event.ports[0].postMessage({
-      version: versionNumber
-    });
+    event.ports[0].postMessage({ version: versionNumber });
   }
 });
