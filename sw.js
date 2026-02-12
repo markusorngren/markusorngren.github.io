8bc87fad3749c1829c5f2c540631993095af06f4
markusorngren <markus.orngren@gmail.com>
2026-02-12 22:09:02 +0100

    Add files via upload
    Uppdaterad konfettin v43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouse-tracker-v42'; // Uppdaterad version för att tvinga fram update
+const CACHE_NAME = 'mouse-tracker-v43'; // Uppdaterad version för att tvinga fram update
 const ASSETS = [
   './',
   './index.html',
