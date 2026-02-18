6f86335e02ed422e5a975809312d7c31ec98b362
markusorngren <markus.orngren@gmail.com>
2026-02-18 22:19:25 +0100

    Add files via upload
    Spara på spara med via punkter v 53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouse-tracker-v52'; // Uppdaterad version för att tvinga fram update
+const CACHE_NAME = 'mouse-tracker-v53'; // Uppdaterad version för att tvinga fram update
 const ASSETS = [
   './',
   './index.html',
