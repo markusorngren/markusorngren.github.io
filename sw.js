0caf0e23f45e3de1d2e624f59a03c5676044aabe
markusorngren <markus.orngren@gmail.com>
2026-02-23 21:30:40 +0100

    Add files via upload
    Ändrat lite text och lagt till swipe instruktion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouse-tracker-v65'; // Uppdaterad version för att tvinga fram update
+const CACHE_NAME = 'mouse-tracker-v66'; // Uppdaterad version för att tvinga fram update
 const MAP_CACHE = 'mouse-map-tiles-v1'; // Ny cache för kartplattor
 const ASSETS = [
   './',
